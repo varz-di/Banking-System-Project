58ed53b7636ad12c45363470ba410b186c8acb8a
Диана Варзина <dianavarzina@Dianas-MacBook-Air.local>
2025-11-28 01:43:07 +0300

    added main.py

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from src.input_handler import InputHandler
+from src.bank import Bank
+from src.account import AccountType
+
+
+if __name__ == "__main__":
+    my_handler = InputHandler()
+    my_handler.run()
